62e6dc01ce5dd82e30928860610b892a8baf1a1d
Jori Bomanson <jori.bomanson@gmail.com>
2020-09-18 10:54:47 +0300

    Run kak separately for each source file

--- lib/send_message.sh
+++ lib/send_message.sh
@@ -1,8 +1,8 @@
 send_message () {
-    if test -w "$KAK_SPEC_DIR/fifo"; then
+    if test -w "$KAK_SPEC_DIR/$KAK_SPEC_INDEX"; then
         {
             printf "%s\n" "$1" | wc -l
             printf "%s\n" "$1"
-        } >"$KAK_SPEC_DIR/fifo"
+        } >"$KAK_SPEC_DIR/$KAK_SPEC_INDEX"
     fi
 }
